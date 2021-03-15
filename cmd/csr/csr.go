2dee48392b3dd5d82428be064e6318c81a25780a
dmakeroam <cpfapp@gmail.com>
2021-03-15 13:16:48 +0700

    - Test resolve secrets with aws provider

--- cmd/csr/csr.go
+++ cmd/csr/csr.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"github.com/kubeopsskills/cloud-secret-resolvers/internal/csr"
+	"github.com/kubeopsskills/cloud-secret-resolvers/internal/pkg/provider"
 )
 
 func main() {
 	// load environment configs
 	var keyValueEnvMap = csr.LoadCredentialKeyFromEnvironment()
+	var cloudType = "aws"
+	switch {
+	case cloudType == "aws":
+		awsProvider := provider.AwsProvider{Region: "ap-southeast-1", SecretName: "prod/test"}
+		csr.SyncCredentialKeyFromCloud(awsProvider, keyValueEnvMap)
+	}
+
 }
--- go.mod
+++ go.mod
@@ -5,3 +5,5 @@ go 1.15
 require github.com/aws/aws-sdk-go v1.37.1
 
 replace github.com/kubeopsskills/cloud-secret-resolvers/internal/csr => /internal/csr
+replace github.com/kubeopsskills/cloud-secret-resolvers/internal/pkg => /internal/pkg
+replace github.com/kubeopsskills/cloud-secret-resolvers/internal/pkg/provider => /internal/pkg/provider
